6cf273c7235087fcb740cb88480651bcb55472e1
Avinash Jat <dev.avinashjat@gmail.com>
2025-10-14 04:08:00 +0530

    create api for remove-bg and object

--- Server/controller/aiController.js
+++ Server/controller/aiController.js
@@ -1,6 +1,8 @@
 import { clerkClient } from "@clerk/express";
 import OpenAI from "openai";
 import sql from "../config/db.js"
+import axios from "axios";
+
 
 import {v2 as cloudinary} from 'cloudinary'
 
@@ -133,12 +135,17 @@ export const generateImage = async (req, res) => {
    const formData = new FormData()
   formData.append('prompt', prompt)
 
-  const {data} = await axios.post('https://clipdrop-api.co/text-to-image/v1', formData , {
-     headers: {
-    'x-api-key': process.env.IMAGE_API_KEY,
-    responseType: 'arraybuffer',
-  }
-})
+   const { data } = await axios.post(
+      "https://clipdrop-api.co/text-to-image/v1",
+      formData,
+      {
+        headers: {
+          "x-api-key": process.env.IMAGE_API_KEY,
+          ...formData.getHeaders?.(),
+        },
+        responseType: "arraybuffer", 
+      }
+    );
 
 const base64Image = `data:image/png;base64,${Buffer.from(data , 'binary').toString('base64')}`;
 
@@ -151,7 +158,51 @@ VALUES (${userId}, ${prompt}, ${secure_url}, 'image' , ${publish ?? false})`;
 
 
 
-   res.json({success : true ,secure_url})
+   res.json({success : true , content:secure_url})
+
+  } catch (error) {
+    console.log(error.message);
+    res.json({success : false ,message: error.message})
+  }
+};
+
+
+
+
+
+
+// Api to remove background from Image
+
+export const removeBackground = async (req, res) => {
+  try {
+   const { userId } = await req.auth();
+   const {image} = req.file;
+    const plan = req.plan;
+
+  if (plan !== "premium") {
+      return res.json({
+        success: false,
+        message: "This feature is available for premium users only. Please upgrade to access it.",
+      });
+    }
+
+const {secure_url} =await cloudinary.uploader.upload(image.path ,{
+     transformation: [
+        {
+          effect : "background_removal",
+          background_removal: "remove_the_background"
+        }
+     ]
+})
+
+
+
+  await sql`INSERT INTO creations (user_id, prompt, content, type)
+VALUES (${userId},'Remove background from image' , ${secure_url}, 'image')`;
+
+
+
+   res.json({success : true ,content : secure_url})
 
   } catch (error) {
     console.log(error.message);
@@ -165,3 +216,39 @@ VALUES (${userId}, ${prompt}, ${secure_url}, 'image' , ${publish ?? false})`;
 
 
 
+
+// Api to remove Object from Image
+
+export const removeObject = async (req, res) => {
+  try {
+   const { userId } = await req.auth();
+    const { object } =  req.body;
+   const {image} = req.file;
+    const plan = req.plan;
+
+  if (plan !== "premium") {
+      return res.json({
+        success: false,
+        message: "This feature is available for premium users only. Please upgrade to access it.",
+      });
+    }
+
+const {public_id} =await cloudinary.uploader.upload(image.path)
+
+const imageUrl = cloudinary.url(public_id, {
+      transformation: [ { effect : `gen_remove:${object}`} ],
+      resource_type: 'image',
+})
+
+
+  await sql`INSERT INTO creations (user_id, prompt, content, type)
+VALUES (${userId},${`Removed ${object} from image`} , ${imageUrl}, 'image')`;
+
+
+   res.json({success : true ,content : imageUrl})
+
+  } catch (error) {
+    console.log(error.message);
+    res.json({success : false ,message: error.message})
+  }
+};
